3f1fb3c48566da9ca24e0da0f47f40bc967e480b
alyonaVet <a.v.alenka2015@gmail.com>
2024-05-28 11:14:00 +0600

    added component Board; added styles;

---
+++ Homework54/src/components/Board/Board.tsx
@@ -0,0 +1,27 @@
+import Cell from '../Cell/Cell.tsx';
+import React from 'react';
+import './Board.css';
+import {BoardCell} from '../../types.ts';
+
+interface Props {
+  items: BoardCell[];
+  toggleCell: (index: number) => void;
+}
+
+const Board: React.FC<Props> = ({items, toggleCell}) => {
+
+  return (
+      <div className="Board">
+        {items.map((item, index) => (
+          <Cell
+            key={index}
+            clicked={item.clicked}
+            toggleCell={item.clicked ? () => null : () => toggleCell(index)}
+            hasItem={item.hasItem}
+            />
+        ))}
+      </div>
+    );
+  };
+
+  export default Board;
